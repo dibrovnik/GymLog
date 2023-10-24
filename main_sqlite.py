67718594fe8bde76e4765f3ad4d0e966872db17e
Nikita Dibrov <dibrovnik@yandex.ru>
2023-10-24 19:16:07 +0300

    clean folders and create file requirements.txt

--- main_sqlite.py
+++ main_sqlite.py
@@ -1,5 +1,3 @@
-import openpyxl
-from openpyxl.styles import Alignment
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types, F
@@ -8,7 +6,7 @@ from aiogram.filters import CommandObject
 from datetime import datetime
 import sqlite3
 
-conn = sqlite3.connect('gym_log.db')
+conn = sqlite3.connect('data\gym_log.db')
 cursor = conn.cursor()
 
 logging.basicConfig(level=logging.INFO) # Включаем логирование, чтобы не пропустить важные сообщения
@@ -60,8 +58,8 @@ def create_user_table(user_id):
 async def cmd_start(message: types.Message):
     kb = [
         [
-            types.KeyboardButton(text="Начать новую тренировку"),
-            types.KeyboardButton(text="Просмотр предыдущих")
+            types.KeyboardButton(text="Начать новую тренировку")
+            # types.KeyboardButton(text="Просмотр предыдущих")
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
@@ -70,18 +68,48 @@ async def cmd_start(message: types.Message):
     )
     await message.answer("привет, я бот для записи твоих тренировок. начнем тренировку?", reply_markup=keyboard)
 
+# @dp.message()
+# async def cmd_finish(message: types.Message):
+#     kb = [
+#         [
+#             types.KeyboardButton(text="Закончить тренировку")
+#             # types.KeyboardButton(text="Просмотр предыдущих")
+#         ],
+#     ]
+#     keyboard = types.ReplyKeyboardMarkup(
+#         keyboard=kb,
+#         resize_keyboard=True,
+#     )
+#     await message.answer('Когда нужно будет завершить тренировку, нажмите на кнопку ниже или напишите "Закончить тренировку"', reply_markup=keyboard)
 
 
-@dp.message(F.text.lower() == "начать новую тренировку")
-async def with_puree(message: types.Message):
-    # await message.reply("пришлите название упражнения, количество повторений и вес на подходе в таком формате: жим лежа, 8, 40")
-
+@dp.message(F.text.lower() == "начать новую тренировку" or "новая")
+async def new_training(message: types.Message):
     await message.reply("Отлично! Пришлите название упражнения, количество повторений и вес на подходе в таком формате: жим лежа, 8, 60", reply_markup=types.ReplyKeyboardRemove())
 
-
+@dp.message(F.text.lower() == "закончить тренировку" or "стоп")
+async def finish_training(message: types.Message):
+    today = datetime.today().strftime('%d-%m-%Y')
+#----------------тут сделать кнопку под сообщением чтобы начать новую тренировку
+    await message.reply(f"Тренировка {today} завершена. Буду ждать тебя снова :)")
 
 @dp.message()
 async def handle_message(message: types.Message):
+
+    # Вывод кнопки завершения
+    kb = [
+        [
+            types.KeyboardButton(text="Закончить тренировку")
+            # types.KeyboardButton(text="Просмотр предыдущих")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+    )
+    await message.answer('Когда нужно будет завершить тренировку, нажмите на кнопку ниже или напишите "Закончить тренировку"', reply_markup=keyboard)
+
+
     # Сохраняем сообщение в переменную
     message_text = message.text
     user_id = message.from_user.id
@@ -99,7 +127,7 @@ async def handle_message(message: types.Message):
 
     name_ex, count, weight = await add_row(message_text)  # Вызываем функцию add_row и сохраняем возвращаемые значения в переменных
 
-    await message.answer(f'Записал данные об упражнении. Название упражнения: {name_ex}, количество повторений: {count} , вес на подходе: {weight}')
+    await message.answer(f'Записал данные об упражнении. Название упражнения: {name_ex}, количество повторений: {count} , вес на подходе: {weight}. Жду следующей записи...')
 
 
 
